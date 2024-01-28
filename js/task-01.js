01a3800cbfcccf57b9a7dfdac91314c2e48798b1
Łukasz Wątorski <watorski.info@gmail.com>
2024-01-28 18:55:51 +0100

    js-hw-06

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,60 @@
-//Zadanue 1
-//Punkt 1 - Policzy i wypisze w konsoli liczbę kategorii w ul#categories, czyli elementy li.item.
+/* Treść Zadania 1
+
+W HTML znajduje się lista kategorii ul#categories.
+
+<ul id="categories">
+  <li class="item">
+    <h2>Animals</h2>
+    <ul>
+      <li>Cat</li>
+      <li>Hamster</li>
+      <li>Horse</li>
+      <li>Parrot</li>
+    </ul>
+  </li>
+  <li class="item">
+    <h2>Products</h2>
+    <ul>
+      <li>Bread</li>
+      <li>Prasley</li>
+      <li>Cheese</li>
+    </ul>
+  </li>
+  <li class="item">
+    <h2>Technologies</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node.js</li>
+    </ul>
+  </li>
+</ul>
+
+
+Napisz skrypt, który:
+1. Policzy i wypisze w konsoli liczbę kategorii w ul#categories, czyli elementy li.item.
+2. Dla każdego elementu li.item na liście ul#categories, znajdzie i wypisze w konsoli tekst nagłówka elementu (tag <h2>) i liczbę elementów w danej kategorii (wszystkich w jej obrębie <li>).
+Po wykonaniu w konsoli znajdą się takie komunikaty.
+
+Number of categories: 3
+
+Category: Animals
+Elements: 4
+
+Category: Products
+Elements: 3
+
+Category: Technologies
+Elements: 5
+
+*/
+
+//Punkt 1
 const categoriesList = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${categoriesList.length}`);
-//Punkt 2 - Dla każdego elementu li.item na liście ul#categories,
-// znajdzie i wypisze w konsoli tekst nagłówka elementu (tag <h2>)
-// i liczbę elementów w danej kategorii (wszystkich w jej obrębie <li>).
+//Punkt 2
 categoriesList.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
   const elementsCount = category.querySelectorAll("ul li").length;
--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,11 @@
+/* Treść Zadania 2
+
+W HTML znajduje się pusta lista ul#ingredients.
+
+<ul id="ingredients"></ul>
+
+W JavaScript znajduje się tablica ciągów.
+
 const ingredients = [
   "Potatoes",
   "Mushrooms",
@@ -7,10 +15,37 @@ const ingredients = [
   "Condiments",
 ];
 
-//Punkt 1. - Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
-//Punkt 2. - Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
-//Punkt 3. - Doda do elementu klasę item.
-//Punkt 4. - Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
+
+Napisz skrypt, który dla każdego elementu tablicy ingredients:
+1. Utworzy oddzielny element <li>. Obowiązkowo użyj metody document.createElement().
+2. Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+3. Doda do elementu klasę item.
+4. Następnie jedna operacja umieści wszystkie <li> na liście ul#ingredients.
+
+*/
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingredientsList = document.getElementById("ingredients");
+
+// nowe
+const items = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return listItem;
+});
+
+ingredientsList.append(...items);
+
+/* stare
 const ingredientsList = document.getElementById("ingredients");
 
 ingredients.forEach((ingredient) => {
@@ -19,3 +54,4 @@ ingredients.forEach((ingredient) => {
   listItem.classList.add("item");
   ingredientsList.appendChild(listItem);
 });
+*/
